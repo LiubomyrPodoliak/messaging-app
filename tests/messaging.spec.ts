bf899d3ce38f16fd54fea1501f25dbe7189f530f
Liubomyr Podoliak <l.podoliak@gmail.com>
2025-07-27 10:19:21 +0300

    clean up

--- tests/messaging.spec.ts
+++ tests/messaging.spec.ts
@@ -10,13 +10,11 @@ test.describe('Messaging App', () => {
     await messagingAppPage.openDummyMessagingApp();
     const testMessages = ['Test 1', 'Test 2', 'Test 3', 'Test 4', 'Test 5'];
     for (const msg of testMessages) {
-      await messagingAppPage.typeMessageWithDelay(msg, 250);
+      await messagingAppPage.typeMessageWithDelay(msg, 100);
       await messagingAppPage.sendMessage();
-     
+
       await messagingAppPage.isMessageDisplayed(msg);
     }
-  
-     
   });
 
   test('Verify User can send a single message', async ({ page, messagingAppPage }) => {
